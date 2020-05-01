043d50d4f129e80be38b88f31e8d8a5881438630
hayaki kento <hayaki@csplan.jp>
2020-05-01 17:31:10 +0900

    todo機能を完成させた

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios"; //追記
+import VueAxios from "vue-axios"; //追記
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state = {
+  auth: {
+    token: null,
+    userId: null,
+  },
+  board: {
+    lists: [],
+  },
+};
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: state,
+  mutations: {},
+  actions: {},
+  modules: {},
+});
